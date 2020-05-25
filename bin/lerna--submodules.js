d75a1bec52c4223b7766ad744c9d09cb5878f8f3
Brian Takita <brian.takita@gmail.com>
2020-05-24 21:03:19 -0700

    fix: lerna--submodules imports

--- bin/lerna--submodules.js
+++ bin/lerna--submodules.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 import cli from '@lerna/cli'
-import version_cli from '@lerna/version/command'
-import publish_cli from '@lerna/publish/command'
 import { lerna_version__submodules, lerna_publish__submodules } from '../lib'
-cli()
-	.command(Object.assign({}, version_cli, {
-		handler: lerna_version__submodules
-	}))
-	.command(Object.assign({}, publish_cli, {
-		handler: lerna_publish__submodules
-	}))
-	.parse(process.argv.slice(2))
+main()
+async function main() {
+	const version_cli = await import('@lerna/version/command')
+	const publish_cli = await import('@lerna/publish/command')
+	cli()
+		.command(Object.assign({}, version_cli, {
+			handler: lerna_version__submodules
+		}))
+		.command(Object.assign({}, publish_cli, {
+			handler: lerna_publish__submodules
+		}))
+		.parse(process.argv.slice(2))
+}
