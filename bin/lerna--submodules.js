3ae4546f3b409a98fdf623a6b110fa2dc8ea3d4a
Brian Takita <brian.takita@gmail.com>
2020-05-24 20:44:14 -0700

    - esm

--- bin/lerna--submodules.js
+++ bin/lerna--submodules.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
-require = require('esm')(module)
-const cli = require('@lerna/cli')
-const version_cli = require('@lerna/version/command')
-const publish_cli = require('@lerna/publish/command')
-const { lerna_version__submodules, lerna_publish__submodules } = require('../lib')
+import cli from '@lerna/cli'
+import version_cli from '@lerna/version/command'
+import publish_cli from '@lerna/publish/command'
+import { lerna_version__submodules, lerna_publish__submodules } from '../lib'
 cli()
 	.command(Object.assign({}, version_cli, {
 		handler: lerna_version__submodules
